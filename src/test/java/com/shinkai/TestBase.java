32b4f4a44c4132f4eb9765e37c52473dcf243d78
Aleksandra Klimantova <alexandra.klimantowa@gmail.com>
2023-03-30 02:51:11 +0200

    added Chrome options

--- src/test/java/com/shinkai/TestBase.java
+++ src/test/java/com/shinkai/TestBase.java
@@ -6,17 +6,20 @@ import com.shinkai.pages.GitHubMainPage;
 import com.shinkai.pages.RepoPage;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestBase {
 
     GitHubMainPage mainPage = new GitHubMainPage();
     RepoPage repoPage = new RepoPage();
     AnnotatedSteps steps = new AnnotatedSteps();
+    ChromeOptions options = new ChromeOptions();
 
     @BeforeEach
     void beforeEach() {
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
         SelenideLogger.addListener("allure", new AllureSelenide());
-        Configuration.browserSize = "1680x1050";
         Configuration.baseUrl = "https://github.com";
     }
 }
